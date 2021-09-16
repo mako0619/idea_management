a0b34d4347ae98e8a204ddbb721fcb0ce61766ab
mako0619 <stardust.shinkan@gmail.com>
2021-09-16 12:37:03 +0900

    RSpecの導入と設定

--- config/application.rb
+++ config/application.rb
@@ -32,6 +32,12 @@ module IdeaManagement
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
 
+    config.generators do |g|
+      g.test_framework :rspec, fixture: false
+      g.controller_specs false
+      g.routing_specs false
+    end
+
     # Only loads a smaller set of middleware suitable for API only apps.
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
