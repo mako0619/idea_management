953e94b86508529e0eb421b76efbe5b3b3741e0f
mako0619 <stardust.shinkan@gmail.com>
2021-09-18 15:22:17 +0900

    関連付けとバリデーションを設定

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,2 +1,14 @@
+# == Schema Information
+#
+# Table name: categories
+#
+#  id         :bigint           not null, primary key
+#  name       :string(255)      not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 class Category < ApplicationRecord
+  has_many :ideas, dependent: :destroy
+  validates :name, uniqueness: true
+  validates :name, presence: true
 end
